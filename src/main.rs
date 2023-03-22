19f2a0f846f36c4bdbd88496d22f04c330a93b01
Alex <lepsalex@gmail.com>
2023-03-21 22:25:22 -0400

    wip - adding ldtk

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
+use bevy_ecs_ldtk::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+mod components;
+mod systems;
+
+pub const WIDTH: f32 = 1280.0;
+pub const HEIGHT: f32 = 720.0;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy 2D Base".to_string(),
+                resolution: (WIDTH, HEIGHT).into(),
+                present_mode: PresentMode::AutoVsync,
+                resizable: false,
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugin(LdtkPlugin)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        // Required to prevent race conditions between bevy_ecs_ldtk's and bevy_rapier's systems
+        .configure_set(LdtkSystemSet::ProcessApi.before(PhysicsSet::SyncBackend))
+        .insert_resource(RapierConfiguration {
+            gravity: Vec2::new(0.0, -2000.0),
+            ..Default::default()
+        })
+        .insert_resource(LevelSelection::Uid(0))
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
+            set_clear_color: SetClearColor::FromLevelBackground,
+            ..Default::default()
+        })
+        .add_startup_system(systems::setup)
+        .add_system(systems::camera_fit_inside_current_level)
+        .run();
 }
+
